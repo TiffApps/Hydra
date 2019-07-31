6cd9eb811abea05b502996d98868db7e2d0a1e8a
Tiffaa <33839996+TiffaaMC@users.noreply.github.com>
2019-07-31 19:01:30 +0200

    Removed useless commented methods

--- src/io/github/tiffaamc/hydra/config/Config.java
+++ src/io/github/tiffaamc/hydra/config/Config.java
@@ -9,8 +9,6 @@ public class Config
 {
     private Config() {}
 
-//    public static String DISCORD_MESSAGE;
-//    public static String FORUMS_MESSAGE;
     public static String NO_PERM_MESSAGE;
     public static String PREFIX_GOOD;
     public static String PREFIX_BAD;
@@ -18,8 +16,6 @@ public class Config
 
     public static void Load(FileConfiguration config)
     {
-//    	DISCORD_MESSAGE = config.getString("Auto.Discord");
-//    	FORUMS_MESSAGE = config.getString("Auto.Forums");
     	NO_PERM_MESSAGE = config.getString("Messages.No-Perm");
         PREFIX_GOOD = config.getString("Messages.Prefix-Good");
         PREFIX_BAD = config.getString("Messages.Prefix-Bad");
@@ -29,9 +25,7 @@ public class Config
 	private static List<String> transList(List<String> list)
     {
         for (int i = 0; i < list.size(); i++)
-        {
             list.set(i, trans(list.get(i)));
-        }
         return list;
     }
 
